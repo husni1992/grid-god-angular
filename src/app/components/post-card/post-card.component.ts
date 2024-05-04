a84f0936adae29cbc110ae30ccc0ee65d053b3a0
H.A.Mohomed Gazzali <mgha123@gmail.com>
2024-05-04 17:19:05 +0200

    Set the post card height dynamically by the width + paddings. Grid background styles applied (for body) - improve..

--- src/app/components/post-card/post-card.component.ts
+++ src/app/components/post-card/post-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { Post } from '../../models/Post.model';
 
 @Component({
@@ -13,14 +13,29 @@ export class PostCardComponent {
   @Input() activePostId: Post['id'] | undefined;
   @Output() setActivePost = new EventEmitter<Post['id']>();
 
+  @ViewChild('postCard') selfElement: ElementRef | undefined;
+
   isActive: boolean = false;
 
   ngOnChanges() {
     this.updateActiveStatus();
   }
 
+  ngAfterViewInit() {
+    this.setSelfElementHeight();
+  }
+
   updateActiveStatus() {
     this.isActive = this.post?.id === this.activePostId;
+  }
+
+  setSelfElementHeight() {
+    if (this.selfElement) {
+      const width = this.selfElement.nativeElement.offsetWidth;
+
+      const padding = parseInt(getComputedStyle(this.selfElement.nativeElement)['paddingTop']);
+      this.selfElement.nativeElement.style.height = `${width - padding * 2}px`;
+    }
   }
 
   onClick() {
